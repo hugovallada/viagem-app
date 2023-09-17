e85b73ecd1f85703527fb6c453900e695d5d9cdf
Hugo <valladahugo@gmail.com>
2023-09-16 22:18:28 -0300

    Adiciona fault tolerance

--- agencia-cli/src/main/java/com/github/hugovallada/cliente/ClienteService.java
+++ agencia-cli/src/main/java/com/github/hugovallada/cliente/ClienteService.java
@@ -1,7 +1,12 @@
 package com.github.hugovallada.cliente;
 
 import jakarta.ws.rs.*;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.time.temporal.ChronoUnit;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -15,5 +20,16 @@ public interface ClienteService {
 
     @GET
     @Path("findById")
+    @Timeout(unit = ChronoUnit.SECONDS, value = 3)
+    @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+            requestVolumeThreshold = 4,
+            failureRatio = 0.5,
+            delay = 6000L,
+            successThreshold = 2)
     Cliente findById(@QueryParam("id") Long id);
+
+    private Cliente fallback(Long id) {
+        return Cliente.of(0, "");
+    }
 }
